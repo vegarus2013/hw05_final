44adf90e6f4ba0fc77036700f9fccabf813b1b31
vegarus2013 <mv-nikolay@vegarus.su>
2022-08-19 01:23:39 +0300

    Add comment and follow

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Group, Post
+from .models import Comment, Follow, Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -22,3 +22,21 @@ class GroupAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Group, GroupAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'created',)
+    search_fields = ('author', 'text',)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Comment, CommentAdmin)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('author', 'user',)
+    search_fields = ('author', 'user',)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Follow, FollowAdmin)

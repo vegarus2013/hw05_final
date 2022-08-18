1d8c74155b52f0fef0a43b65e2948b8b0e3f73df
vegarus2013 <mv-nikolay@vegarus.su>
2022-08-19 00:16:20 +0300

    Add Comment

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Post
+from .models import Comment, Post
 
 
 class PostForm(forms.ModelForm):
@@ -10,7 +10,16 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('text', 'group')
+        fields = ['text', 'group', 'image']
         widgets = {
             'text': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'cols': 60, 'rows': 5}),
         }

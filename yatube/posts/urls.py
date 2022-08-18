65e73c1625c2149a484f889bc361531256b6eb5d
vegarus2013 <mv-nikolay@vegarus.su>
2022-08-19 00:16:09 +0300

    Add Comment and Follow

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -15,4 +15,12 @@ urlpatterns = [
     path('create/', views.post_create, name='post_create'),
     # Редактирование записей
     path('posts/<post_id>/edit/', views.post_edit, name='post_edit'),
+    # Комментария
+    path('posts/<post_id>/comment/', views.add_comment, name='add_comment'),
+    # Подписки
+    path('follow/', views.follow_index, name='follow_index'),
+    path('profile/<username>/follow/',
+         views.profile_follow, name='profile_follow'),
+    path('profile/<username>/unfollow/',
+         views.profile_unfollow, name='profile_unfollow'),
 ]

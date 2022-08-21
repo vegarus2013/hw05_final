917fbc9c14bc92337eb09162c2cc6d61c87aed89
vegarus2013 <mv-nikolay@vegarus.su>
2022-08-21 17:03:30 +0300

    The test must pass: version 3.2

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -174,7 +174,7 @@ class PaginatorViewsTest(TestCase):
         for response in responses:
             with self.subTest(response=response):
                 self.assertEqual(
-                    len(response.context['page_obj']),
+                    len(response.context['page_obj'].object_list),
                     settings.PAGE_SIZE)
 
     def test_last_page_content(self):
@@ -188,7 +188,7 @@ class PaginatorViewsTest(TestCase):
         for response in responses:
             with self.subTest(response=response):
                 self.assertEqual(
-                    len(response.context['page_obj']), 1)
+                    len(response.context['page_obj'].object_list), 1)
 
 
 class FollowViewsTest(TestCase):
